3442968424a2058c12bb84e53aba4b9fbdfc01c2
Zahan Malkani <zahanm@gmail.com>
2012-01-29 15:58:38 -0800

    correct factor vars for Singleton

--- PA3/ComputeSingletonFactors.m
+++ PA3/ComputeSingletonFactors.m
@@ -24,9 +24,9 @@ factors = repmat(struct('var', [], 'card', [], 'val', []), n, 1);
 % Your code here:
 
 for i = 1:n;
-  factors(i).var = 1;
+  factors(i).var = i;
   factors(i).card = imageModel.K;
-  factors(i).val = ComputeImageFactor( images(i), imageModel );
+  factors(i).val = ComputeImageFactor( images(i).img, imageModel );
 end
 
 end

208d70ef0fdf0affa0b42f5cdccb973b58d070a1
Zahan Malkani <zahanm@gmail.com>
2012-03-04 14:49:53 -0800

    learn graph and cpds

--- PA8/LearnGraphAndCPDs.m
+++ PA8/LearnGraphAndCPDs.m
@@ -10,21 +10,23 @@ function [P G loglikelihood] = LearnGraphAndCPDs(dataset, labels)
 %         the ith example belongs to class j
     
 N = size(dataset, 1);
-K = size(labels,2);
+K = size(labels, 2);
 
 G = zeros(10,2,K); % graph structures to learn
 % initialization
 for k=1:K
-    G(2:end,:,k) = ones(9,2);
+  G(2:end,:,k) = ones(9,2);
 end
 
 % estimate graph structure for each class
 for k=1:K
-    % fill in G(:,:,k)
-    % use ConvertAtoG to convert a maximum spanning tree to a graph G
-    %%%%%%%%%%%%%%%%%%%%%%%%%
-    % YOUR CODE HERE
-    %%%%%%%%%%%%%%%%%%%%%%%%%
+  % fill in G(:,:,k)
+  % use ConvertAtoG to convert a maximum spanning tree to a graph G
+  %%%%%%%%%%%%%%%%%%%%%%%%%
+  % YOUR CODE HERE
+  %%%%%%%%%%%%%%%%%%%%%%%%%
+  [A W] = LearnGraphStructure(dataset(labels(:,k) == 1, :, :));
+  G(:,:,k) = ConvertAtoG(A);
 end
 
 % estimate parameters
@@ -37,5 +39,7 @@ P.c = zeros(1,K);
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[P loglikelihood] = LearnCPDsGivenGraph(dataset, G, labels);
 
 fprintf('log likelihood: %f\n', loglikelihood);

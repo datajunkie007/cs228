4cda71f0a0d812dab1247b41a1931ef01b58655e
Zahan Malkani <zahanm@gmail.com>
2012-03-04 12:14:50 -0800

    naming clearer

--- PA8/ComputeLogLikelihood.m
+++ PA8/ComputeLogLikelihood.m
@@ -23,7 +23,7 @@ function loglikelihood = ComputeLogLikelihood(P, G, dataset)
 N = size(dataset,1); % number of examples
 K = length(P.c); % number of classes
 
-loglikelihood = 0;
+loglikelihood = -Inf;
 
 % You should compute the log likelihood of data as in eq. (12) and (13)
 % in the PA description
@@ -37,8 +37,8 @@ loglikelihood = 0;
 for i=1:N
 
   for k=1:K
-    l1 = log(P.c(k));
-    l2 = 0;
+    lpk = log(P.c(k));
+    lpoi = 0;
 
     for part = 1:10
 
@@ -50,20 +50,20 @@ for i=1:N
       end
 
       if ( parentpart == 0 )
-        l2 = l2 + lognormpdf( dataset(i, part, 1), P.clg(part).mu_y(k), P.clg(part).sigma_y(k) );
-        l2 = l2 + lognormpdf( dataset(i, part, 2), P.clg(part).mu_x(k), P.clg(part).sigma_x(k) );
-        l2 = l2 + lognormpdf( dataset(i, part, 3), P.clg(part).mu_angle(k), P.clg(part).sigma_angle(k) );
+        lpoi = lpoi + lognormpdf( dataset(i, part, 1), P.clg(part).mu_y(k), P.clg(part).sigma_y(k) );
+        lpoi = lpoi + lognormpdf( dataset(i, part, 2), P.clg(part).mu_x(k), P.clg(part).sigma_x(k) );
+        lpoi = lpoi + lognormpdf( dataset(i, part, 3), P.clg(part).mu_angle(k), P.clg(part).sigma_angle(k) );
       else
         mu_y = P.clg(part).theta(k, 1) + P.clg(part).theta(k, 2) * dataset(i, parentpart, 1) + P.clg(part).theta(k, 3) * dataset(i, parentpart, 2) + P.clg(part).theta(k, 4) * dataset(i, parentpart, 3);
         mu_x = P.clg(part).theta(k, 5) + P.clg(part).theta(k, 6) * dataset(i, parentpart, 1) + P.clg(part).theta(k, 7) * dataset(i, parentpart, 2) + P.clg(part).theta(k, 8) * dataset(i, parentpart, 3);
         mu_angle = P.clg(part).theta(k, 9) + P.clg(part).theta(k, 10) * dataset(i, parentpart, 1) + P.clg(part).theta(k, 11) * dataset(i, parentpart, 2) + P.clg(part).theta(k, 12) * dataset(i, parentpart, 3);
-        l2 = l2 + lognormpdf( dataset(i, part, 1), mu_y, P.clg(part).sigma_y(k) );
-        l2 = l2 + lognormpdf( dataset(i, part, 2), mu_x, P.clg(part).sigma_x(k) );
-        l2 = l2 + lognormpdf( dataset(i, part, 3), mu_angle, P.clg(part).sigma_angle(k) );
+        lpoi = lpoi + lognormpdf( dataset(i, part, 1), mu_y, P.clg(part).sigma_y(k) );
+        lpoi = lpoi + lognormpdf( dataset(i, part, 2), mu_x, P.clg(part).sigma_x(k) );
+        lpoi = lpoi + lognormpdf( dataset(i, part, 3), mu_angle, P.clg(part).sigma_angle(k) );
       end
     end
 
-    loglikelihood = log( exp(loglikelihood) + exp(l1 + l2) );
+    loglikelihood = log( exp(loglikelihood) + exp(lpk + lpoi) );
   end
 
 end

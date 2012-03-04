116d3421a6f2adf38fcadd744928714eae08e55c
Zahan Malkani <zahanm@gmail.com>
2012-03-04 14:29:16 -0800

    learning graph structure

--- PA8/LearnGraphStructure.m
+++ PA8/LearnGraphStructure.m
@@ -13,16 +13,26 @@ function [A W] = LearnGraphStructure(dataset)
 % W: 10 x 10 weight matrix, where W(i,j) is the mutual information between
 %    node i and j. 
 
-N = size(dataset,1);
-K = size(dataset,3);
+N = size(dataset, 1);
+K = size(dataset, 3);
 
-W = zeros(10,10);
+numparts = size(dataset, 2);
+W = zeros(numparts);
+
 % Compute weight matrix W
 % set the weights following Eq. (14) in PA description
 % you don't have to include M since all entries are scaled by the same M
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE        
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for i = 1:numparts
+  for j = 1:numparts
+    Oi = dataset(:, i, :);
+    Oj = dataset(:, j, :);
+    W(i,j) = N * GaussianMutualInformation(Oi, Oj);
+  end
+end
 
 % Compute maximum spanning tree
 A = MaxSpanningTree(W);
